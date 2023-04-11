421e0ce536964636ef6630ebe0249794a44b5c8b
rajendraswooshelf <102212866+rajendraswooshelf@users.noreply.github.com>
2023-04-11 12:39:27 +0530

    Update admin.py

--- sangharsh-project/new_app/admin.py
+++ sangharsh-project/new_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from new_app.models import BloodDonate, Contact, CustomUser, Activity, FoundationAccountSetting
-
+ 
 # Register your models here.
 
 admin.site.register(CustomUser)
